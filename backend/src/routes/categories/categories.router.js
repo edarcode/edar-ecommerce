40fc60281556124f3874f690a13836594ffdffef
Edwin Ortiz <edarcode@gmail.com>
2022-02-03 04:07:24 -0500

    feat(backend/src/routes/categories/putController/updateCategory.js) actulizar categoria

--- backend/src/routes/categories/categories.router.js
+++ backend/src/routes/categories/categories.router.js
@@ -2,10 +2,12 @@ const { Router } = require("express");
 const deleteCategory = require("./deleteController/deleteCategory.controller");
 const getCategory = require("./getController/getCategories.controller");
 const createCategory = require("./postController/createCategory.controller");
+const updateCategory = require("./putController/updateCategory");
 const categories = Router();
 
 categories.route("/").post(createCategory);
 categories.route("/").get(getCategory);
 categories.route("/").delete(deleteCategory);
+categories.route("/").put(updateCategory);
 
 module.exports = categories;
