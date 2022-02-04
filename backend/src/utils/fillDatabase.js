dc90a7797f735a03547af81d1327d188a9142b5f
Edwin Ortiz <edarcode@gmail.com>
2022-02-04 08:01:45 -0500

    feat(backend/src/utils/fillDatabase.js) creando productos cuando levanta el server auto...

--- backend/src/utils/fillDatabase.js
+++ backend/src/utils/fillDatabase.js
@@ -1,5 +1,6 @@
 const { categories } = require("../mockups/categories.json");
 const { suppliers } = require("../mockups/suppliers.json");
+const { products } = require("../mockups/products.json");
 const { axiosPost } = require("./axios");
 const { baseURL } = process.env;
 
@@ -19,6 +20,16 @@ module.exports = {
       for (let i = 0; i < suppliers.length; i++) {
         const element = suppliers[i];
         await axiosPost(`${baseURL}/suppliers`, element);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  },
+  loadMockProducts: async function () {
+    try {
+      for (let i = 0; i < products.length; i++) {
+        const element = products[i];
+        await axiosPost(`${baseURL}/products`, element);
       }
     } catch (error) {
       console.log(error);
