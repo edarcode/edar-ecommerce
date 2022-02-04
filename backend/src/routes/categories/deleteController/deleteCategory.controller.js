66460cd9a93243e7286b0358509d21f2bbb9470a
Edwin Ortiz <edarcode@gmail.com>
2022-02-04 03:30:14 -0500

    refactor(backend/src/routes/categories/deleteController/deleteCategory.controller.js)

--- backend/src/routes/categories/deleteController/deleteCategory.controller.js
+++ backend/src/routes/categories/deleteController/deleteCategory.controller.js
@@ -3,8 +3,10 @@ const { Category } = require("../../../db");
 const deleteCategory = async (req, res, next) => {
   try {
     const { id } = req.query;
-    const result = await Category.destroy({ where: { id } }); //return 0 o 1
-    res.json(result ? { msg: "Destroy successfully" } : { msg: "Not found" });
+    const isDestroy = await Category.destroy({ where: { id } }); //return 0 o 1
+    res.json(
+      isDestroy ? { msg: "Destroy successfully" } : { msg: "Not found" }
+    );
   } catch (error) {
     next(error);
   }
