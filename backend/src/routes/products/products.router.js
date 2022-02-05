5e34208bc73a44e2f18a384a140b5cfaae91a2b9
Edwin Ortiz <edarcode@gmail.com>
2022-02-04 22:20:34 -0500

    feat(backend/src/routes/products/deleteController/deleteProduct.controller.js) eliminar producto

--- backend/src/routes/products/products.router.js
+++ backend/src/routes/products/products.router.js
@@ -1,9 +1,11 @@
 const { Router } = require("express");
+const deleteProduct = require("./deleteController/deleteProduct.controller");
 const getProducts = require("./getController/getProducts.controller");
 const createProduct = require("./postController/createProduct.controller");
 const products = Router();
 
 products.route("/").post(createProduct);
 products.route("/").get(getProducts);
+products.route("/").delete(deleteProduct);
 
 module.exports = products;
