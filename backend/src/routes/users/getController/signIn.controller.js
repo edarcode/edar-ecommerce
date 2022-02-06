dd460ae13d46641170216b7a6751056f1c52f6d8
Edwin Ortiz <edarcode@gmail.com>
2022-02-06 07:31:48 -0500

    feat(backend/src/routes/users/getController/signIn.controller.js) autenticación

--- backend/src/routes/users/getController/signIn.controller.js
+++ backend/src/routes/users/getController/signIn.controller.js
@@ -5,7 +5,16 @@ const { SECRET } = process.env;
 
 const signIn = async (req, res, next) => {
   try {
-    res.json({ msg: "signin" });
+    const { password, email } = req.body;
+    const user = await User.findOne({ where: { email } });
+    if (!user) return res.json({ msg: "Not found" });
+    const isLogin = await comparePassword(password, user.password);
+    if (!isLogin) {
+      res.json({ msg: "Not found" });
+    } else {
+      const token = jwt.sign({ id: user.id }, SECRET, { expiresIn: "24h" });
+      res.json({ token });
+    }
   } catch (error) {
     next(error);
   }
