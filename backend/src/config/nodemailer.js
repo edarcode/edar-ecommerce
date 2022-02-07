6c3872b6924d8c6d9779b0dfdd168490dc17fba1
Edwin Ortiz <edarcode@gmail.com>
2022-02-07 05:42:48 -0500

    feat(backend/src/config/) nodemailer

---
+++ backend/src/config/nodemailer.js
@@ -0,0 +1,14 @@
+const nodemailer = require("nodemailer");
+export const transporter = nodemailer.createTransport({
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true, // true for 465, false for other ports
+  auth: {
+    user: "testedarcode@gmail.com", // generated ethereal user
+    pass: "qcluahmhzvnqplhq", // generated ethereal password
+  },
+});
+
+transporter.verify().then(() => {
+  console.log("Ready for send emails");
+});
