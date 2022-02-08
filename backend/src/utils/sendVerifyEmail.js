f5b5d22f4164c870c6fb002a0ecf442c0853aa7f
Edwin Ortiz <edarcode@gmail.com>
2022-02-08 02:45:12 -0500

    refactor(backend/src/utils/sendVerifyEmail.js)

--- backend/src/utils/sendVerifyEmail.js
+++ backend/src/utils/sendVerifyEmail.js
@@ -1,15 +1,16 @@
 const { SECRET } = process.env;
 const jwt = require("jsonwebtoken");
 const transporter = require("../config/nodemailer");
+const { baseURL } = process.env;
 
 module.exports = {
   sendVerifyEmail: async ({ user, expiresIn }) => {
     const token = jwt.sign({ id: user.id }, SECRET, { expiresIn });
     await transporter.sendMail({
-      from: '"verify email 👻" <testedarcode@gmail.com>', // sender address
+      from: '"👻 Verify email 👻" <testedarcode@gmail.com>', // sender address
       to: user.email, // list of receivers
-      subject: "verify email ✔", // Subject line
-      text: `http://localhost:3001/users/verify/email?token=${token}`, // plain text body
+      subject: "✔ Verify email ✔", // Subject line
+      text: `${baseURL}/users/verify/email?token=${token}`, // plain text body
       //html: "<b>Hello world?</b>", // html body
     });
   },
