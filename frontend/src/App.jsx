572f58c4968f84ac0cd8aad2bc1d7d0e39883206
Edwin Ortiz <edarcode@gmail.com>
2022-02-16 01:55:40 -0500

    feat(frontend/src/App.jsx) init routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,27 @@
 import { AppSc } from "./AppSc";
+import { Routes, Route } from "react-router-dom";
+import Home from "./views/Home/Home";
+import Cart from "./views/Cart/Cart";
+import Catalogue from "./views/Catalogue/Catalogue";
+import Contact from "./views/Contact/Contact";
+import Dashboard from "./views/Dashboard/Dashboard";
+import Product from "./views/Product/DetailProduct";
+import Login from "./views/Login/Login";
+import Store from "./views/Store/Store";
 
 function App() {
   return (
     <AppSc className="App">
-      <h1>hola mundo</h1>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/catalogue" element={<Catalogue />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/product" element={<Product />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/store" element={<Store />} />
+      </Routes>
     </AppSc>
   );
 }
---
+++ frontend/src/consts/colors.js
@@ -0,0 +1,3 @@
+export const bgWebsite = "#16161a";
+export const bgBox = "#2d2d2d";
+export const lyrics = "#94a1b2";
