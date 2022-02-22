3df0269ba9fca893fe03be01b0769027267cd1c8
Edwin Ortiz <edarcode@gmail.com>
2022-02-22 18:40:54 -0500

    refactor(frontend/src/redux/reducers/products/actions.js)

--- frontend/src/redux/reducers/products/actions.js
+++ frontend/src/redux/reducers/products/actions.js
@@ -3,45 +3,35 @@ import { axiosGet } from "../../../utils/axios";
 import { SET_PRODUCTS } from "./const";
 
 export const getAllProducts = (filters) => {
-  const { page, name, idCategory, min, max, order } = filters;
   return async (dispatch) => {
-    let url = "/products";
-    const isPage = filters.hasOwnProperty("page");
-    const isMin = filters.hasOwnProperty("min") && min >= 0;
-    if (isPage && name && idCategory && isMin && max && order) {
-      url = `/products?page=${page}&name=${name}&idCategory=${idCategory}&min=${min}&max=${max}&order=${order}`;
-    } else if (isPage && name && idCategory && isMin && max) {
-      url = `/products?page=${page}&name=${name}&idCategory=${idCategory}&min=${min}&max=${max}`;
-    } else if (isPage && name && idCategory && order) {
-      url = `/products?page=${page}&name=${name}&idCategory=${idCategory}&order=${order}`;
-    } else if (isPage && name && isMin && max && order) {
-      url = `/products?page=${page}&name=${name}&min=${min}&max=${max}&order=${order}`;
-    } else if (isPage && name && idCategory) {
-      url = `/products?page=${page}&name=${name}&idCategory=${idCategory}`;
-    } else if (isPage && name && isMin && max) {
-      url = `/products?page=${page}&name=${name}&min=${min}&max=${max}`;
-    } else if (isPage && name && order) {
-      url = `/products?page=${page}&name=${name}&order=${order}`;
-    } else if (isPage && idCategory && isMin && max && order) {
-      url = `/products?page=${page}&idCategory=${idCategory}&min=${min}&max=${max}&order=${order}`;
-    } else if (isPage && idCategory && isMin && max) {
-      url = `/products?page=${page}&idCategory=${idCategory}&min=${min}&max=${max}`;
-    } else if (isPage && idCategory && order) {
-      url = `/products?page=${page}&idCategory=${idCategory}&order=${order}`;
-    } else if (isPage && isMin && max && order) {
-      url = `/products?page=${page}&min=${min}&max=${max}&order=${order}`;
-    } else if (isPage && name) {
-      url = `/products?page=${page}&name=${name}`;
-    } else if (isPage && idCategory) {
-      url = `/products?page=${page}&idCategory=${idCategory}`;
-    } else if (isPage && isMin && max) {
-      url = `/products?page=${page}&min=${min}&max=${max}`;
-    } else if (isPage && order) {
-      url = `/products?page=${page}&order=${order}`;
-    } else if (isPage) {
-      url = `/products?page=${page}`;
-    }
+    const url = addQuery(filters);
     const allDataProducts = await axiosGet({ url });
     dispatch(action(SET_PRODUCTS, allDataProducts));
   };
 };
+
+function addQuery(filters) {
+  const { page, name, idCategory, min, max, order } = filters;
+  let url = "/products";
+  const isPage = filters.hasOwnProperty("page");
+  const isMin = filters.hasOwnProperty("min") && min >= 0;
+  if (isPage) {
+    url += `?page=${page}`;
+  }
+  if (name) {
+    url += `&name=${name}`;
+  }
+  if (idCategory) {
+    url += `&idCategory=${idCategory}`;
+  }
+  if (isMin) {
+    url += `&min=${min}`;
+  }
+  if (max) {
+    url += `&max=${max}`;
+  }
+  if (order) {
+    url += `&order=${order}`;
+  }
+  return url;
+}
